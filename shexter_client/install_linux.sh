49c913798cec9824b359e90ca13e308322fe904f
allan2 <allan@azic.ca>
2016-06-28 16:34:16 -0400

    addressed: 'Failed to create symbolic link... File exists'

--- shexter_client/install_linux.sh
+++ shexter_client/install_linux.sh
@@ -1,29 +1,20 @@
 #!/bin/sh
 
 #short script to 'install' shexter
-#copies shexter.py to /opt/, then the shexter executable to /usr/bin.
-#needs sudo
+#copies shexter.py to /opt/, then symlinks the shexter executable to /usr/bin.
 
 if [ "$(id -u)" != "0" ]; then
-	echo "You must run this installer as sudo since it writes to /usr/bin and /opt/"
+	echo "You must run this installer with root permissions for access to /opt/ and /usr/bin/"
 	exit 1
 fi
 
-if [ -f /opt/shexter/shexter.py ]; then
-	rm /opt/shexter/shexter.py
+if [ -f /usr//bin/shexter ]; then
+	echo "Removing existing symlink at /usr/bin/shexter"
+	rm /usr/bin/shexter
 fi
-if [ -f /usr/local/bin/shexter ]; then
-	rm /usr/local/bin/shexter
-fi
-
-# not necessary with mkdir -p
-#if [ ! -d /opt/ ]; then
-#	mkdir /opt/
-#fi
 
 mkdir -p /opt/shexter && cp ./shexter.py "$_" && cp ./shexter "$_"
 ln -s /opt/shexter/shexter /usr/bin/shexter
-
 
 if [ -f /opt/shexter/shexter.py ] && [ -f /usr/bin/shexter ]; then
 	chmod a+x /opt/shexter/shexter
