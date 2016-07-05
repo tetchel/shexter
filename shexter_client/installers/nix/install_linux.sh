db19aa4fbadf120cdbef3c027099b10ccefe35b1
Noah Murad <noahmurad@hotmail.com>
2016-07-05 00:43:59 -0400

    installer checks if files were updated

--- shexter_client/installers/nix/install_linux.sh
+++ shexter_client/installers/nix/install_linux.sh
@@ -17,16 +17,21 @@ OPT_DIR='/opt/shexter/'
 
 # $_ should have worked but it did not
 mkdir -p $OPT_DIR && cp ../../shexter.py $OPT_DIR && 
-        cp ../../lib/appdirs.py $OPT_DIR && cp ./shexter $OPT_DIR
+       cp ../../lib/appdirs.py $OPT_DIR && cp ./shexter $OPT_DIR
 
 ln -s $OPT_DIR"shexter" /usr/bin/shexter
 
+# get dates of files to see if they were updated
+NOW=`date +"%Y%m%d%H%M%S"`
+FDATE1=`date -r $OPT_DIR"shexter.py" +"%Y%m%d%H%M%S"`
+FDATE2=`date -r $OPT_DIR"appdirs.py" +"%Y%m%d%H%M%S"`
+
 if [ -f $OPT_DIR"shexter.py" ]  && [ -f $OPT_DIR"appdirs.py" ] && 
-        [ -f /usr/bin/shexter ]; then
+        [ -f /usr/bin/shexter ] && [ `expr $NOW - $FDATE1` -lt 5 ] &&
+        [ `expr $NOW - $FDATE2` -lt 5 ] ; then
 	
     chmod a+x /opt/shexter/shexter
-    # TODO The problem with this is that it will print success even if it failed if
-    # files existed before. so should also check their last modified time.
+
     echo "Success!"
 else
 	echo "Something went wrong."
