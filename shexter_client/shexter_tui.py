0665d207447f3d9dccfe0a599bcae9d6cf4b9f87
Noah Murad <noahmurad@hotmail.com>
2016-08-03 21:17:10 -0400

    started on a class for scrolling pads

--- shexter_client/shexter_tui.py
+++ shexter_client/shexter_tui.py
@@ -1,6 +1,54 @@
 import curses
 import atexit
 
+#-----------Scrolly Pad-----------
+class ScrollyPad:
+
+    def __init__(self, h, w, start_y, start_x, end_y, end_x):
+        self.height = h
+        self.width = w
+        self.y1 = start_y
+        self.x1 = start_x
+        self.y2 = end_y
+        self.x2 = end_x
+        self.current_line = 0
+        self.pad = curses.newpad(self.height,self.width)
+
+    def grow_from_bottom(self):
+        for i in range(0,self.height):
+            self.pad.addch(ord('\n'))
+
+    # shorthands #
+    def infinite_scroll(self):
+        self.pad.scrollok(True)
+
+    def addstr(self, string):
+        try:
+            self.pad.addstr(string)
+        except curses.error:
+            pass
+
+    def addstr(self, string, attr):
+        try:
+            self.pad.addstr(string,attr)
+        except curses.error:
+            pass
+
+    def addstr(self, y, x, string):
+        try:
+            self.pad.addstr(y,x,string)
+        except curses.error:
+            pass
+
+    def addstr(self, y, x, string, attr):
+        try:
+            self.pad.addstr(y,x,string,attr)
+        except curses.error:
+            pass
+
+
+#-----------Functions-------------
+
 # gracefully close curses
 def close_curses():
     curses.echo()
@@ -51,9 +99,11 @@ def get_convo_header(name):
 # TODO get convo date slapped into here
 # TODO put end of convo at bottom of screen (not maybe return a value to allow this from this function, for the refresh)
 def create_convo():
-    convo = curses.newpad(100, max_x())
+    convo = curses.newpad(max_y()-recents_height-msg_height-1, max_x())
+    convo.scrollok(True)
     try:
-        convo.addstr(0, 0, "T: sup foo\n", curses.color_pair(2))
+        convo.addstr(0,0, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        convo.addstr("T: sup foo\n", curses.color_pair(2))
         convo.addstr("N: nm bruh, jst chiln\n")
         convo.addstr("T: Bruh bruh asdlfkjasdfl;kjasdfjklahsdflkjahsdflkjhasdflkjhasdflkjhasdflkjhasdflkjhasdflkjhasdflkjhasdflkjahsdflkjahsdflkjahsdflkjahsdflkajhsdf end of long thing (hey it wrapped!!)\n", curses.color_pair(2))
         convo.addstr("N: This should block your wrapped message, T!! :P\n")
@@ -66,7 +116,7 @@ def create_convo():
 
     return convo
 
-# create a pad to hold a typed message
+# create a pad ta static method doesn't know its class or instanceo hold a typed message
 # TODO pad should grow as message is typed (*cough* noutrefesh doupdate *cough*)
 def create_message():
     message = curses.newpad(100, max_x())
@@ -80,8 +130,8 @@ def create_message():
 # ----------------MAlN-------------------
 
 # constants
-RECENTS_HEIGHT = 3
-MSG_HEIGHT = 0
+recents_height = 3
+msg_height = 0
 
 # init curses
 stdscr = curses.initscr()
@@ -92,7 +142,7 @@ atexit.register(close_curses)
 set_colors()
 
 # set up decorations
-stdscr.addstr(RECENTS_HEIGHT+1, 0,get_convo_header("Time"), curses.color_pair(2)) 
+stdscr.addstr(recents_height+1, 0,get_convo_header("Time"), curses.color_pair(2)) 
 
 # set up recents pad
 recents = create_recents()
@@ -114,9 +164,9 @@ except curses.error:
 
 # refresh screens
 stdscr.noutrefresh()
-recents.noutrefresh(0,0, 0,0, RECENTS_HEIGHT,max_x())
-convo.noutrefresh(0,0, RECENTS_HEIGHT+2,0, max_y()-MSG_HEIGHT-1,max_x())
-message.noutrefresh(0,0, max_y()-1-MSG_HEIGHT,0, max_y()-1,max_x())
+recents.noutrefresh(0,0, 0,0, recents_height,max_x())
+convo.noutrefresh(0,0, recents_height+2,0, max_y()-msg_height-1,max_x())
+message.noutrefresh(0,0, max_y()-1-msg_height,0, max_y()-1,max_x())
 curses.doupdate()
 
 stdscr.getch()
