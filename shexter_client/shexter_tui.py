21f7d628336a6d69348de788f4f2c449807939d3
Noah Murad <noahmurad@hotmail.com>
2016-08-05 17:02:53 -0400

    fully integrated ScrollyPads

--- shexter_client/shexter_tui.py
+++ shexter_client/shexter_tui.py
@@ -13,6 +13,29 @@ class ScrollyPad:
         self.x2 = end_x
         self.current_line = 0
         self.pad = curses.newpad(self.height,self.width)
+        self.hidden = False
+
+    def refresh(self):
+        if not self.hidden:
+            self.pad.noutrefresh(self.current_line,0, self.y1,self.x1, self.y2,self.x2)
+
+    def screen_height(self):
+        return self.y2 - self.y1 + 1
+
+    # scrolly stuff
+    def scroll_up(self):
+        if self.current_line > self.top():
+            self.current_line -= 1
+
+    def scroll_down(self):
+        if self.current_line < self.bottom():
+            self.current_line += 1
+
+    def top(self):
+        return 0
+
+    def bottom(self):
+        return self.height - self.screen_height()
 
     def cursor_to_bottom(self):
         for i in range(0,self.height):
@@ -21,13 +44,10 @@ class ScrollyPad:
             except curses.error:
                 pass
 
-    def refresh(self):
-        self.pad.noutrefresh(self.current_line,0, self.y1,self.x1, self.y2,self.x2)
-
     def infinite_scroll(self):
         self.pad.scrollok(True)
         self.cursor_to_bottom()
-        self.current_line = self.height - self.y2 + self.y1 - 1
+        self.current_line = self.bottom()
 
     # shorthand 
     def addstr(self, a1, a2=None, a3=None, a4=None):
@@ -71,7 +91,7 @@ def set_colors():
 
 def max_x():
     y,x = stdscr.getmaxyx()
-    return x#
+    return x
 
 def max_y():
     y,x = stdscr.getmaxyx()
@@ -81,17 +101,17 @@ def max_y():
 # uses 
 # TODO get recent convo data slapped into here
 def create_recents():
-    recents = curses.newpad(100, max_x())
-    try:
-        recents.addstr(0, 0, "3* Test Man")
-        recents.addstr(1, 0, "2* First Last")
-        recents.addstr(2, 0, "2* John Doe")
-        recents.addstr(3, 0, " * Joe Doe")
-        recents.addstr(4, 0, " * Mr. Not Visible Without Scroll")
-        recents.addstr(5, 0, " * Sir")
-        recents.addstr(6, 0, " * Buzz Lightyear")
-    except curses.error:
-        pass
+    # init
+    recents = ScrollyPad(100, max_x(), 0,0, 3,max_x())
+    
+    # set text
+    recents.addstr(0, 0, "3* Test Man")
+    recents.addstr(1, 0, "2* First Last")
+    recents.addstr(2, 0, "2* John Doe")
+    recents.addstr(3, 0, " * Joe Doe")
+    recents.addstr(4, 0, " * Mr. Not Visible Without Scroll")
+    recents.addstr(5, 0, " * Sir")
+    recents.addstr(6, 0, " * Buzz Lightyear")
 
     return recents
 
@@ -102,13 +122,11 @@ def get_convo_header(name):
 
 # pad which holds the current conversation
 # uses a newline at the end of each message so that each message is fully displayed
-# TODO get convo date slapped into here
-# TODO put end of convo at bottom of screen (not maybe return a value to allow this from this function, for the refresh)
+# TODO get convo data slapped into here
 def create_convo():
     # init
-    convo = ScrollyPad(max_y()-recents_height-msg_height-1,max_x(), recents_height+2,0, max_y()-msg_height-1,max_x()) #TODO replace height with 1000 or something and set current_line appropriately
+    convo = ScrollyPad(1000,max_x(), recents.screen_height()+1,0, max_y()-message.screen_height(),max_x()) #TODO +1 is for the decorations. Maybe integrate the decorations into convo 
     convo.infinite_scroll()
-    convo.cursor_to_bottom()
 
     # set text
     convo.addstr("T: sup foo\n", curses.color_pair(2))
@@ -124,18 +142,18 @@ def create_convo():
 # create a pad ta static method doesn't know its class or instanceo hold a typed message
 # TODO pad should grow as message is typed (*cough* noutrefesh doupdate *cough*)
 def create_message():
-    message = curses.newpad(100, max_x()) 
-    try:
-        message.addstr(0,0, "N: ", curses.A_BOLD)
-    except curses.error:
-        pass
+    # init
+    message = ScrollyPad(100, max_x(), max_y()-1,0, max_y()-1,max_x()) 
+    message.infinite_scroll()
+
+    # set text
+    message.addstr("N: ", curses.A_BOLD)
 
     return message
 
 # ----------------MAlN-------------------
 
 # constants
-recents_height = 3
 msg_height = 0
 
 # init curses
@@ -146,32 +164,19 @@ stdscr.keypad(1)
 #atexit.register(close_curses)
 set_colors()
 
-# set up decorations
-stdscr.addstr(recents_height+1, 0,get_convo_header("Time"), curses.color_pair(2)) 
-
-# set up recents pad
+# create pads
 recents = create_recents()
-
-# set up conversation pad
-convo = create_convo()
-
-# set up messaging pad
 message = create_message()
+convo = create_convo()
+stdscr.addstr(recents.screen_height(), 0,get_convo_header("Time"), curses.color_pair(2)) 
 
-# set up curses interface (ie tests on stdscr)
-try:
-    stdscr.addstr(6, 0, str(max_y()) + " by " +  str(max_x()), curses.A_BOLD)
-    stdscr.addstr(7, 0, "A hectic hello to Curses!!!", curses.A_STANDOUT)
-    stdscr.addstr(8, 0, "sup from me", curses.color_pair(1))
-    stdscr.addstr(9, 0, "sup from them", curses.color_pair(2))
-except curses.error:
-    pass
+# set up curses interface (ie tests on stdscr) stdscr.addstr(7, 0, "A hectic hello to Curses!!!", curses.A_STANDOUT)
 
 # refresh screens
 stdscr.noutrefresh()
-recents.noutrefresh(0,0, 0,0, recents_height,max_x())
+recents.refresh()
 convo.refresh()
-message.noutrefresh(0,0, max_y()-1-msg_height,0, max_y()-1,max_x())
+message.refresh()
 curses.doupdate()
 
 stdscr.getch()
